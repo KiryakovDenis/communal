30109cc76b2dfffe2a61a618c78fa212e44e1898
Kiryakov Denis <KiryakovDenis@gmail.com>
2019-01-04 16:57:12 +0500

    Добавлены модели поставщиков и периодов

--- communal/historyPay/models.py
+++ communal/historyPay/models.py
@@ -1,3 +1,33 @@
 from django.db import models
 
-# Create your models here.
+class Period(models.Model):
+    name = models.CharField(max_length=50, help_text="Наименование периода")
+    begin_d = models.DateField(help_text='Дата начала периода')
+    end_d   = models.DateField(help_text='Дата окончанияе периода')
+    comment = models.TextField(max_length=500, help_text='Примечание', blank=True, null=True)
+
+    class Meta:
+        ordering = ["-begin_d"]
+ 
+    def get_absolute_url(self):
+        return reverse('model-detail-view', args=[str(self.id)])
+    def __repr__(self):
+        return 'Period(name=%r, begin_d=%r, end_d=%r)' %(self.name, self.begin_d, self.end_d)
+    def __str__(self):
+        return 'Период [%s - %s]' % (self.begin_d, self.end_d)
+
+class ServiceProvider(models.Model):
+    name = models.CharField(max_length=50, help_text="Наименование поставщика услуг")
+
+    class Meta:
+        ordering = ["name"]
+    def get_absolute_url(self):
+        return reverse('model-detail-view', args=[str(self.id)])
+    def __repr__(self):
+        return 'ServiceProvider(name=%r)' % (self.name)
+    def __str__(self):
+        return 'Поставщик %s' % (self.name)
+
+class Pay(models.Model):
+    pass 
+    
--- communal/historyPay/urls.py
+++ communal/historyPay/urls.py
@@ -3,5 +3,8 @@ from django.urls import path, include
 from . import views
 
 urlpatterns = [
-    path('home', views.home, name='HomePage'),    
+    #path('home', views.home, name='HomePage'),      
+    #path('', views.home, name='HomePage'), 
+    path('home/', views.home.as_view(), name='HomePage'),
+    path('', views.home.as_view(), name='HomePage'),
 ]

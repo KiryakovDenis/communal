ebbe006513fcaddd85f2c025dfd70b9b7756a450
Kiryakov Denis <KiryakovDenis@gmail.com>
2019-01-18 16:44:40 +0500

    Добавлен bootstrap 4 создан каркас клиентской части

--- communal/historyPay/urls.py
+++ communal/historyPay/urls.py
@@ -2,9 +2,10 @@ from django.contrib import admin
 from django.urls import path, include
 from . import views
 
-urlpatterns = [
-    #path('home', views.home, name='HomePage'),      
-    #path('', views.home, name='HomePage'), 
+urlpatterns = [    
+    path('PayList/', views.PayList.as_view(), name='PayList'),
+    path('PeriodList/', views.PeriodList.as_view(), name='PeriodList'),
+    path('serviceProviderList/', views.serviceProviderList.as_view(), name='serviceProviderList'),
     path('home/', views.home.as_view(), name='HomePage'),
     path('', views.home.as_view(), name='HomePage'),
 ]
--- communal/historyPay/views.py
+++ communal/historyPay/views.py
@@ -2,15 +2,22 @@ from django.shortcuts import render
 from django.http import HttpResponse
 from django.views.generic import View
 
-# Create your views here.
-
-#def home(request):
-#    return render(request, 'historyPay/HomePage.html', {})
 class home(View):
-    def get(self, request):
-        #return HttpResponse('Result')
+    def get(self, request):        
         return render(request, 'historyPay/HomePage.html', {})
 
 class login(View):
     def get(self, request):
         return render(request, 'registrations/login.html', {})
+
+class serviceProviderList(View):
+    def get(self, request):        
+        return render(request, 'historyPay/serviceProviderList.html', {})
+
+class PeriodList(View):
+    def get(self, request):        
+        return render(request, 'historyPay/PeriodList.html', {})
+
+class PayList(View):
+    def get(self, request):        
+        return render(request, 'historyPay/PayList.html', {})
